081ee3edf296c4755bcce124f9bf2dbc2c017c12
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2022-06-16 11:08:19 -0500

    update scatter plot and data

--- behavior_tracker.py
+++ behavior_tracker.py
@@ -6,7 +6,15 @@ excel_file = 'malachi_data_tracker.xlsx'
 df = pd.read_excel(excel_file)
 print(df)
 
-data = [go.Scatter( x=df['Behavior'], y=df['Date'])]
+size = [20, 40, 60, 80, 100, 80, 60, 40, 20, 40]
+
+data = [go.Scatter( x=df['Behavior'], y=df['Possible Trigger'], mode='markers',
+        marker=dict(
+        size=size,
+        sizemode='area',
+        sizeref=2.*max(size)/(40.**2),
+        sizemin=4))]
+
 
 fig = go.Figure(data)
 fig.show()
