8b42f16a60fa2d6aa19876acebe57289aa37be1e
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2022-06-16 10:56:46 -0500

    input behavior and date data

--- behavior_tracker.py
+++ behavior_tracker.py
@@ -5,3 +5,8 @@ import pandas as pd
 excel_file = 'malachi_data_tracker.xlsx'
 df = pd.read_excel(excel_file)
 print(df)
+
+data = [go.Scatter( x=df['Behavior'], y=df['Date'])]
+
+fig = go.Figure(data)
+fig.show()
